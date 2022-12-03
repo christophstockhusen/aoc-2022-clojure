2e9fdde3180089f06a90dd528f2e08365284f02c
Christoph Stockhusen <mail@christophstockhusen.de>
2022-12-03 09:16:59 +0100

    Licensing

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject aoc-2022-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Advent of Code 2022"
+  :url "https://github.com/christophstockhusen/aoc-2022-clojure"
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns aoc-2022-clojure.core})
